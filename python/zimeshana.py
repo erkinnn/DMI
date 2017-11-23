fff65f42b290b90aad1e4a40c80a555e4fa7845a
kreaux77@gmail.com <erkinnn@inbox.lv>
2017-11-23 17:19:47 +0200

    20171123_17_09

---
+++ python/zimeshana.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3, 4])
+
+#plt.plot([1,2,3,4])
+
+plt.plot([-2,1], [4,-3] ,'*')
+
+plt.xlable('some other numbers')
+plt.ylabel('some numbers')
+
+plt.show()
